680bd745c66386124a85a50eee5efedd09470020
strange-dv <andrii.dovzhenko.dv@gmail.com>
2022-01-16 22:43:18 +0200

    code reformatted

--- public_identifiers/fields.py
+++ public_identifiers/fields.py
@@ -13,13 +13,13 @@ class ISBNField(fields.CharField):
     description = _("The International Standard Book Number")
 
     def __init__(self, verbose_name=None, name=None, **kwargs):
-        kwargs.setdefault('max_length', 13)
+        kwargs.setdefault("max_length", 13)
         super().__init__(verbose_name, name, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         if kwargs.get("max_length") == 13:
-            del kwargs['max_length']
+            del kwargs["max_length"]
         return name, path, args, kwargs
 
 
@@ -34,13 +34,13 @@ class DOIField(fields.CharField):
     def __init__(self, verbose_name=None, name=None, **kwargs):
         # it is hard to tell the max length of DOI,
         # but it should be less than 255
-        kwargs.setdefault('max_length', 255)
+        kwargs.setdefault("max_length", 255)
         super().__init__(verbose_name, name, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         if kwargs.get("max_length") == 255:
-            del kwargs['max_length']
+            del kwargs["max_length"]
         return name, path, args, kwargs
 
 
@@ -53,11 +53,11 @@ class ISSNField(fields.CharField):
     description = _("The International Standard Serial Number")
 
     def __init__(self, verbose_name=None, name=None, **kwargs):
-        kwargs.setdefault('max_length', 9)
+        kwargs.setdefault("max_length", 9)
         super().__init__(verbose_name, name, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         if kwargs.get("max_length") == 9:
-            del kwargs['max_length']
+            del kwargs["max_length"]
         return name, path, args, kwargs
--- public_identifiers/validators.py
+++ public_identifiers/validators.py
@@ -6,18 +6,18 @@ class ISBNValidator(validators.RegexValidator):
     # example: 978-1-56619-909-4
 
     regex = r"^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$"
-    message = _('Enter a valid ISBN.')
+    message = _("Enter a valid ISBN.")
 
 
 class DOIValidator(validators.RegexValidator):
     # example: 10.1111/j.1365-2575.2012.00413.x
 
     regex = r"^.*(10\.[A-Za-z0-9.\/-]+)(?<!\.)(?=[ ]|\.).*$"
-    message = _('Enter a valid DOI.')
+    message = _("Enter a valid DOI.")
 
 
 class ISSNValidator(validators.RegexValidator):
     # example: 0208-6336
 
     regex = r"^[\S]{4}\-[\S]{4}$"
-    message = _('Enter a valid ISSN.')
+    message = _("Enter a valid ISSN.")
